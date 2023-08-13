12c16ed6f78a6052562a3fe73dc247802726d5a7
Danvintius <danila.94@mail.ru>
2023-08-13 17:12:12 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { ShopItemClass } from './components/ShopItemClass';
 
-function App() {
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+export function App() {
   return (
-    <div className="container">
-    <div className="background-element">
+    <div class="container">
+    <div class="background-element">
     </div>
-    <div className="highlight-window">
-      <div className='highlight-overlay'></div>
+    <div class="highlight-window">
+      <div class='highlight-overlay'></div>
     </div>
-    <div className="window">
+    <div class="window">
       <ShopItemClass item={item} />
     </div>
   </div>
